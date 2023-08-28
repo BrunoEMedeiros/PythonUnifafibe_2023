ad05baa3be57a10d5dd9f8bf1f56ea38333ffdf3
D3adSec <bruno.emedeiros99@gmail.com>
2023-08-28 19:41:16 -0300

    listas, dicionarios e tuplas

---
+++ aula26_08.py
@@ -0,0 +1,56 @@
+matriz = [{
+            "codigo" : 1,
+            "nome" : 'bruno',
+            "idade" : 24
+        },
+        {
+            "codigo" : 2,
+            "nome" : 'andre',
+            "idade" : 12
+        },
+        {
+            "codigo" : 3,
+            "nome" : 'maria',
+            "idade" : 18
+        }
+        ]
+
+# for registro in matriz:
+#     if 'andre' in registro["nome"]:
+#         registro["nome"] = 'luiz'
+#         break
+
+# print(matriz)
+
+# for linha in matriz:
+#     if 'sla' in linha:
+#         linha[1] = 'joao'
+#         break
+
+# print(matriz)
+
+# for linha in matriz:
+#     for posicao, valor in enumerate(linha):
+#         if valor == 'bruno':
+#             linha[posicao] = 'andre'
+#             break
+
+#     print(linha)
+
+#-------------------Dicionario----------------------
+dicionario = {
+    "nome" : "buno",
+    "idade" : 24,
+    "profissao" : "desenvolvedor"
+}
+
+# for chave, valor in dicionario.items():
+#     print(chave, valor)
+
+#dicionario["nome"] = "andre"
+#dicionario.update({"codigo" : 1})
+#dicionario.pop("idade")
+#lista = ['a','b','c']
+#novo = dicionario.fromkeys(lista, 1)
+
+
---
+++ teste.js
@@ -0,0 +1,6 @@
+
+let numeros = [1,2,3,4,5]
+
+numeros.map((n)=>{
+    console.log(n)
+})
